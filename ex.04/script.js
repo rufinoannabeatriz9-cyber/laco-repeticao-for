9811917c354610f8bc9da26c1e8cde3cd1f37742
rufinoannabeatriz9 <rufinoannabeatriz9@gmail.com>
2026-04-29 12:42:44 -0300

    Exiba 5 Notas para fazer a média

--- ex.04/script.js
+++ ex.04/script.js
@@ -4,6 +4,5 @@ for (let i = 1; i <= 5; i++) {
     let nota = Number(prompt("Digite a " + i + "ª nota:"));
     totalNotas += nota;
 }
-
 let media = totalNotas / 5;
 alert("A média das notas é: " + media.toFixed(2));
